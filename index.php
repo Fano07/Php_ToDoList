5cd4985991adff0675e57039cd13355b5f781ee8
Fano07 <minf.steph@wanadoo.fr>
2016-06-27 15:10:41 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
 <?php
 
-$Id = $_GET['tokena'];//je récupère l'id dans l'url !
-$login = $_GET['tokenb'];//je récupère le login dans l'url !
-
-if (!Id==0) {
-$Bienvenue = "<h4>Connexion ".$login."</h4>";
-} else {
-  $Bienvenue=null;
-}
-
-?>
-
-<html>
-<head>
-
-<!--METAS ICI-->
-<?php include_once "fragments/metas.php";?>
-
-</head>
-<body>
-
-<!--ENTETE ICI-->
-<?php include_once "fragments/entete.php";?>
-
-<!--CONTENU ICI-->
-<?php include_once "fragments/contenu.php";?>
-
-<!--PIED DE PAGE ICI-->
-<?php include_once "fragments/footer.php";?>
-
-
-</body>
-</html>
+ ?>
+ 
+ <h2>Vous &eagrave;tes arriv&eacute; dans la back-office</h2>
